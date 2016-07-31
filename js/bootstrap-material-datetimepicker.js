0903536c5395d123148464f9fec528fc361a87c6
Steven Mirabito <steven@stevenmirabito.com>
2016-07-31 00:49:25 -0400

    Add support for AMD and CommonJS/Node module systems

--- js/bootstrap-material-datetimepicker.js
+++ js/bootstrap-material-datetimepicker.js
@@ -1,4 +1,44 @@
-(function ($, moment)
+(function (factory)
+{
+   if (typeof define === 'function' && define.amd)
+   {
+      // AMD. Register as an anonymous module.
+      define(['jquery', 'moment'], factory);
+   }
+   else if (typeof module === 'object' && module.exports)
+   {
+      // Node/CommonJS
+      module.exports = function(root, jQuery, moment)
+      {
+         if (jQuery === undefined)
+         {
+            // require('jQuery') returns a factory that requires window to
+            // build a jQuery instance, we normalize how we use modules
+            // that require this pattern but the window provided is a noop
+            // if it's defined (how jquery works)
+            if (typeof window !== 'undefined')
+            {
+               jQuery = require('jquery');
+            }
+            else {
+               jQuery = require('jquery')(root);
+            }
+         }
+
+         if (moment === undefined)
+         {
+            moment = require('moment');
+         }
+
+         factory(jQuery, moment);
+         return jQuery;
+      };
+   }
+   else {
+      // Browser globals
+      factory(jQuery, moment);
+   }
+}(function ($, moment)
 {
    var pluginName = "bootstrapMaterialDatePicker";
    var pluginDataName = "plugin_" + pluginName;
@@ -1174,4 +1214,4 @@
               }
 
            };
-})(jQuery, moment);
+}));
